54b0356fce6206de163e794376c5fe49be25d297
brainthief <vytautas1983@gmail.com>
2019-04-03 21:17:50 +0300

    created components

--- src/App.js
+++ src/App.js
@@ -2,12 +2,35 @@ import React from 'react';
 import './App.css';
 
 const App = () => {
-
-  return (
+  return (<>
+    <Header />
     <div className="App">
       <h1>SOCNET</h1>
     </div>
+    <Technologies />
+  </>
   );
 }
+
+const Header = () => {
+  return (
+    <>
+      <a href="#s">Home</a>  {" | "}
+      <a href="#s">News Feed</a>  {" | "}
+      <a href="#s">Messages</a>
+    </>
+  );
+}
+
+const Technologies = () => {
+  return (
+    <ul>
+      <li>css</li>
+      <li>html</li>
+      <li>js</li>
+      <li>react</li>
+    </ul>
+  )
+};
 
 export default App;
