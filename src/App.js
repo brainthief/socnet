da3058d78079db316f28d07c1b59166b5782e4ca
Vytautas Vasiliauskas <vytautas1983@gmail.com>
2019-04-04 16:53:25 +0300

    grid for main page

--- src/App.js
+++ src/App.js
@@ -2,23 +2,23 @@ import React from 'react';
 import './App.css';
 
 const App = () => {
-  return (<>
-    <header>
-      <img src="https://www.freelogodesign.org/Content/img/logo-ex-7.png" alt="logo" />
-    </header>
-    <nav>
-      <div>Profile</div>
-      <div>Message</div>
-      <div>News</div>
-      <div>Music</div>
-      <div>Settings</div>
-    </nav>
-    <div className="App">
-      <h1>SOCNET</h1>
-      <p>Main content</p>
+  return (
+    <div className="appWrapper">
+      <header className="header">
+        <img src="https://www.freelogodesign.org/Content/img/logo-ex-7.png" alt="logo" />
+      </header>
+      <nav className="nav">
+        <div>Profile</div>
+        <div>Message</div>
+        <div>News</div>
+        <div>Music</div>
+        <div>Settings</div>
+      </nav>
+      <div className="content">
+        <h1>SOCNET</h1>
+        <p>Main content</p>
+      </div>
     </div>
-
-  </>
   );
 }
 
