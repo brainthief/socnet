3d2c32f4589fae5eda9fa4d96776ecc09cbc0a28
brainthief <vytautas1983@gmail.com>
2019-04-11 20:41:00 +0300

    navbar changed a to NavLink. fixed page refreshing

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,8 +5,8 @@ import { NavLink } from 'react-router-dom'
 const Nav = () => {
  return (
   <nav className={css.nav}>
-   <div className={css.item}><NavLink to="/profile" className={css.item} activeClassName={css.active}>Profile</NavLink></div>
-   <div className={`${css.item} ${css.active}`}><NavLink to="/dialogs" activeClassName={css.active}>Message</NavLink></div>
+   <div className={css.item}><NavLink to="/profile" activeClassName={css.active}>Profile</NavLink></div>
+   <div className={css.item}><NavLink to="/dialogs" activeClassName={css.active}>Message</NavLink></div>
    <div className={css.item}><NavLink to="/news" activeClassName={css.active}>News</NavLink></div>
    <div className={css.item}><NavLink to="/music" activeClassName={css.active}>Music</NavLink></div>
    <div className={css.item}><NavLink to="/settings" activeClassName={css.active}>Settings</NavLink></div>
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -3,7 +3,19 @@ import css from './Profile.module.css'
 import MyPosts from "./MyPosts/MyPosts"
 
 const Profile = (props) => {
- const profilePage = props.profilePage;
+ // const profilePage = props.profilePage;
+
+ const profilePage = {
+  title: 'My posts',
+  comments: [
+   ['all ok ;)', '5'],
+   [':D', '0'],
+   [':)', '10'],
+   ['I am alone', '57'],
+   ['How are you', '17'],
+   [`Its 'my first post`, '7']
+  ]
+ }
  return (
   <div className={css.content}>
    <div className={css.contentText}>
