ccded0216122620b95f68eb1c42ad870d0d2c8a3
brainthief <vytautas1983@gmail.com>
2019-04-11 20:41:27 +0300

    added dialogs navigation

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,17 +1,32 @@
 import React from 'react'
 import css from './Dialogs.module.css'
+import { NavLink } from 'react-router-dom'
 
 const Dialogs = (props) => {
- const { userList, chat } = props.dialogPage;
+ // const { userList, chat } = props.dialogPage;
+
+ const userList = ['Vytautas', 'Timas', 'Darius', 'Jonas', 'Kęstas']
+ const chat = [
+  ['Me', 'Hi! How are you?'],
+  ['User 1', `It's ok. How are you?`],
+  ['Me', 'All right.'],
+  ['Me', 'How was your day'],
+  ['User 1', `All ok`],
+  ['User 1', `Work hard`],
+  ['User 1', `Came home`],
+  ['User 1', `Learning programing`],
+ ];
  return (
   <div className={css.content}>
    <div className={css.contentText}>
     <div className={css.dialogWrap}>
      <div className={css.peopleList}>
-      <div className={css.active}>{userList[0]}</div>
-      <div>{userList[1]}</div>
-      <div>{userList[2]}</div>
-      <div>{userList[3]}</div>
+      <div><NavLink to='/dialogs/0' activeClassName={css.active} >{userList[0]} </NavLink></div>
+      <div><NavLink to='/dialogs/1' activeClassName={css.active} >{userList[1]} </NavLink></div>
+      <div><NavLink to='/dialogs/2' activeClassName={css.active} >{userList[2]} </NavLink></div>
+      <div><NavLink to='/dialogs/3' activeClassName={css.active} >{userList[3]} </NavLink></div>
+      <div><NavLink to='/dialogs/4' activeClassName={css.active} >{userList[4]} </NavLink></div>
+      <div><NavLink to='/dialogs/5' activeClassName={css.active} >{userList[5]} </NavLink></div>
      </div>
      <div className={css.messageList}>
       <div className={css.msgMy}>
