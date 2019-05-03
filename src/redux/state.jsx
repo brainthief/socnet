d41823c16c9ddc32d3ed7095f0b490f0bfbde8ed
Vytautas Vasiliauskas <vytautas1983@gmail.com>
2019-05-03 20:37:11 +0300

    added store

--- src/redux/state.jsx
+++ src/redux/state.jsx
@@ -1,43 +1,64 @@
-export const state = {
- profilePage: {
-  title: 'My posts',
-  comments: [
-   { msg: 'all ok ;)', likeCount: 5 },
-   { msg: ':D', likeCount: 0 },
-   { msg: ':)', likeCount: 10 },
-   { msg: 'I am alone', likeCount: 57 },
-   { msg: 'How are you', likeCount: 17 },
-   { msg: `Its 'my first post`, likeCount: 7 }
-  ],
-  newComment: ""
+const store = {
+ _state: {
+  profilePage: {
+   title: 'My posts',
+   comments: [
+    { msg: 'all ok ;)', likeCount: 5 },
+    { msg: ':D', likeCount: 0 },
+    { msg: ':)', likeCount: 10 },
+    { msg: 'I am alone', likeCount: 57 },
+    { msg: 'How are you', likeCount: 17 },
+    { msg: `Its 'my first post`, likeCount: 7 }
+   ],
+   newComment: ""
+  },
+  dialogPage: {
+   userList: [
+    { id: '0', name: 'Vytas' },
+    { id: '1', name: 'Vytautas' },
+    { id: '2', name: 'Tomas' },
+    { id: '3', name: 'Darius' },
+    { id: '4', name: 'Kęstas' },
+    { id: '5', name: 'Julius' },
+   ],
+   chat: [
+    { id: "1", author: 'Me', msg: 'Hi! How are you?', me: "1" },
+    { id: "2", author: 'User 1', msg: `It's ok. How are you?`, me: "0" },
+    { id: "3", author: 'Me', msg: 'All right.', me: "1" },
+    { id: "4", author: 'Me', msg: 'How was your day', me: "1" },
+    { id: "5", author: 'User 1', msg: `All ok`, me: "0" },
+    { id: "6", author: 'User 1', msg: `Work hard`, me: "0" },
+    { id: "7", author: 'User 1', msg: `Came home`, me: "0" },
+    { id: "8", author: 'User 1', msg: `Learning programing`, me: "0" },
+   ],
+   newMessage: ""
+  },
+  sideBar: {
+   friends: [
+    { id: 0, name: "Vaida", imgUrl: "vaida.JPG" },
+    { id: 1, name: "Viltė", imgUrl: "vilte.JPG" },
+    { id: 2, name: "Vinksė", imgUrl: "vinkse.JPG" },
+   ]
+  }
  },
- dialogPage: {
-  userList: [
-   { id: '0', name: 'Vytas' },
-   { id: '1', name: 'Vytautas' },
-   { id: '2', name: 'Tomas' },
-   { id: '3', name: 'Darius' },
-   { id: '4', name: 'Kęstas' },
-   { id: '5', name: 'Julius' },
-  ],
-  chat: [
-   { id: "1", author: 'Me', msg: 'Hi! How are you?', me: "1" },
-   { id: "2", author: 'User 1', msg: `It's ok. How are you?`, me: "0" },
-   { id: "3", author: 'Me', msg: 'All right.', me: "1" },
-   { id: "4", author: 'Me', msg: 'How was your day', me: "1" },
-   { id: "5", author: 'User 1', msg: `All ok`, me: "0" },
-   { id: "6", author: 'User 1', msg: `Work hard`, me: "0" },
-   { id: "7", author: 'User 1', msg: `Came home`, me: "0" },
-   { id: "8", author: 'User 1', msg: `Learning programing`, me: "0" },
-  ],
-  newMessage: ""
+ getState() {
+  return this._state
  },
- sideBar: {
-  friends: [
-   { id: 0, name: "Vaida", imgUrl: "vaida.JPG" },
-   { id: 1, name: "Viltė", imgUrl: "vilte.JPG" },
-   { id: 2, name: "Vinksė", imgUrl: "vinkse.JPG" },
-  ]
+ dispatch(action) {
+  switch (action.type) {
+   case "ADD_COMMENT":
+    this._state.profilePage.comments = [{ msg: this._state.profilePage.newComment, likeCount: '0' }, ...this._state.profilePage.comments]
+    this._state.profilePage.newComment = ''
+    this._refresh()
+    break;
+   default:
+    break;
+  }
+ },
+ _refresh() {
+ },
+ subscribe(func) {
+  this._refresh = func
  }
 }
 
@@ -48,25 +69,25 @@ export const setRender = (job) => {
 }
 
 export const addNewPost = () => {
- state.profilePage.comments = [{ msg: state.profilePage.newComment, likeCount: '0' }, ...state.profilePage.comments]
- state.profilePage.newComment = ''
+ store._state.profilePage.comments = [{ msg: store._state.profilePage.newComment, likeCount: '0' }, ...store._state.profilePage.comments]
+ store._state.profilePage.newComment = ''
  render();
 }
 
 export const changeTextArea = (e) => {
- state.profilePage.newComment = e.target.value
+ store._state.profilePage.newComment = e.target.value
  render();
 }
 
 export const changeMessageTextArea = (e) => {
- state.dialogPage.newMessage = e.target.value
+ store._state.dialogPage.newMessage = e.target.value
  render();
 }
 
 export const addNewMessage = () => {
- state.dialogPage.chat = [...state.dialogPage.chat, { id: state.dialogPage.chat.length + 1, author: 'Me', msg: state.dialogPage.newMessage, me: "1" }]
- state.dialogPage.newMessage = ''
+ store._state.dialogPage.chat = [...store._state.dialogPage.chat, { id: store._state.dialogPage.chat.length + 1, author: 'Me', msg: store._state.dialogPage.newMessage, me: "1" }]
+ store._state.dialogPage.newMessage = ''
  render();
 }
 
-export default state
+export default store
