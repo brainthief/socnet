2539e7d94003abfc36a9b9eb024b752dd3db6c63
Vytautas Vasiliauskas <vytautas1983@gmail.com>
2019-05-09 09:50:20 +0300

    refactoring

--- src/redux/state.jsx
+++ src/redux/state.jsx
@@ -1,77 +1,28 @@
-export const ADD_COMMENT = 'PROFILE/ADD_COMMENT'
-export const UPDATE_TEXT = 'PROFILE/UPDATE_TEXT'
-export const ADD_MESSAGE = 'DIALOGS/ADD_MESSAGE'
-export const UPDATE_MESSAGE = 'DIALOGS/UPDATE_MESSAGE'
+
+import profilePageReducer from './../redux/profilePageReducer'
+import dialogPageReducer from './../redux/dialogPageReducer'
+import sideBarReducer from './../redux/sideBarReducer'
+
+export const ADD_COMMENT = 'SC/PROFILE_PAGE/ADD_COMMENT'
+export const UPDATE_TEXT = 'SC/PROFILE_PAGE/UPDATE_TEXT'
+export const ADD_MESSAGE = 'SC/DIALOG_PAGE/ADD_MESSAGE'
+export const UPDATE_MESSAGE = 'SC/DIALOG_PAGE/UPDATE_MESSAGE'
 
 const store = {
   _state: {
-    profilePage: {
-      title: 'My posts',
-      comments: [
-        { msg: 'all ok ;)', likeCount: 5 },
-        { msg: ':D', likeCount: 0 },
-        { msg: ':)', likeCount: 10 },
-        { msg: 'I am alone', likeCount: 57 },
-        { msg: 'How are you', likeCount: 17 },
-        { msg: `Its 'my first post`, likeCount: 7 }
-      ],
-      newComment: ""
-    },
-    dialogPage: {
-      userList: [
-        { id: '0', name: 'Vytas' },
-        { id: '1', name: 'Vytautas' },
-        { id: '2', name: 'Tomas' },
-        { id: '3', name: 'Darius' },
-        { id: '4', name: 'Kęstas' },
-        { id: '5', name: 'Julius' },
-      ],
-      chat: [
-        { id: "1", author: 'Me', msg: 'Hi! How are you?', me: "1" },
-        { id: "2", author: 'User 1', msg: `It's ok. How are you?`, me: "0" },
-        { id: "3", author: 'Me', msg: 'All right.', me: "1" },
-        { id: "4", author: 'Me', msg: 'How was your day', me: "1" },
-        { id: "5", author: 'User 1', msg: `All ok`, me: "0" },
-        { id: "6", author: 'User 1', msg: `Work hard`, me: "0" },
-        { id: "7", author: 'User 1', msg: `Came home`, me: "0" },
-        { id: "8", author: 'User 1', msg: `Learning programing`, me: "0" },
-      ],
-      newMessage: ""
-    },
-    sideBar: {
-      friends: [
-        { id: 0, name: "Vaida", imgUrl: "vaida.JPG" },
-        { id: 1, name: "Viltė", imgUrl: "vilte.JPG" },
-        { id: 2, name: "Vinksė", imgUrl: "vinkse.JPG" },
-      ]
-    }
+    profilePage: profilePageReducer(undefined, { type: 'init' }),
+    dialogPage: dialogPageReducer(undefined, { type: 'init' }),
+    sideBar: sideBarReducer(undefined, { type: 'init' })
   },
   getState() {
     return this._state
   },
   dispatch(action) {
-    switch (action.type) {
-      case ADD_COMMENT:
-        this._state.profilePage.comments = [{ msg: this._state.profilePage.newComment, likeCount: '0' }, ...this._state.profilePage.comments]
-        this._state.profilePage.newComment = ''
-        this._refresh()
-        break;
-      case UPDATE_TEXT:
-        this._state.profilePage.newComment = action.value
-        this._refresh()
-        break
-      case ADD_MESSAGE:
-        this._state.dialogPage.chat = [...this._state.dialogPage.chat, { id: this._state.dialogPage.chat.length + 1, author: 'Me', msg: this._state.dialogPage.newMessage, me: "1" }]
-        this._state.dialogPage.newMessage = ''
-        this._refresh();
-        break
-      case UPDATE_MESSAGE:
-        this._state.dialogPage.newMessage = action.value
-        this._refresh()
-        break
-      default:
-        break;
-    }
+    this._state.profilePage = profilePageReducer(this._state.profilePage, action)
+    this._state.dialogPage = dialogPageReducer(this._state.dialogPage, action)
+    this._state.sideBar = sideBarReducer(this._state.sideBar, action)
+
+    this._refresh();
   },
   _refresh() {
   },
