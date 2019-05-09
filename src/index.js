2214346b7d63a07313cdc152124c3fe0fc9787b7
Vytautas Vasiliauskas <vytautas1983@gmail.com>
2019-05-09 09:50:10 +0300

    refactoring to use redux

--- src/index.js
+++ src/index.js
@@ -3,20 +3,35 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import store from './redux/state.jsx';
+// import store from './redux/state.jsx';
+
+import profilePageReducer from './redux/profilePageReducer'
+import dialogPageReducer from './redux/dialogPageReducer'
+import sideBarReducer from './redux/sideBarReducer'
+import { combineReducers, createStore } from 'redux'
+
+
+const combinedReducers = combineReducers({
+ profilePage: profilePageReducer,
+ dialogPage: dialogPageReducer,
+ sideBar: sideBarReducer
+})
+const store = createStore(combinedReducers)
 const state = store.getState()
 
-const renderEntirePage = () => {
+const renderEntirePage = (state) => {
  ReactDOM.render(<App
   state={state}
   dispatch={store.dispatch.bind(store)}
  />, document.getElementById('root'));
 }
 
-store.subscribe(renderEntirePage);
-// setRender(renderEntirePage)
+store.subscribe(() => {
+ const state = store.getState();
+ renderEntirePage(state)
+});
 
-renderEntirePage();
+renderEntirePage(state);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
