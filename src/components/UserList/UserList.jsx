324cfdb538e455494bcf5035aee0ed6d718efca1
brainthief <vytautas1983@gmail.com>
2019-06-03 22:16:52 +0300

    refactored added userlist componentdidmount

--- src/components/UserList/UserList.jsx
+++ src/components/UserList/UserList.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-
 import API from './../../api/API'
 import { connect } from "react-redux";
 import css from './UserList.module.css'
@@ -12,22 +11,42 @@ import {
 import logo from './../../img/incognito.png'
 
 class UserList extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    // this.props.setData = this.props.setData.bind(this)
+  }
+
+  componentDidMount() {
+    console.log(this.props)
+    API.getUsers().then(res => {
+      // debugger
+      res.data.error ? alert(res.data.error) : this.props.setData(res.data)
+      res.data.error ? alert(res.data.error) : this.props.setSuccess()
+    });
+
+
+  }
+
   render() {
-    const { status, data } = this.props.userList
-    const { setInprogress, setSuccess, setData } = this.props
+    // const { status, data } = this.props.userList
+    // const { setInprogress, setSuccess, setData } = this.props
 
-    if (status === statuses.NOT_INITIALIZED) {
-      setInprogress();
-      API.getUsers().then(res => {
-        res.data.error ? alert(res.data.error) : setData(res.data)
-        res.data.error ? alert(res.data.error) : setSuccess()
-      });
 
-    }
+
+    // if (status === statuses.NOT_INITIALIZED) {
+    //   setInprogress();
+    //   API.getUsers().then(res => {
+    //     res.data.error ? alert(res.data.error) : setData(res.data)
+    //     res.data.error ? alert(res.data.error) : setSuccess()
+    //   });
+
+    // }
 
     return <div className={css.contentText}>
       User List
-    {status === statuses.SUCCESS ? data.items.map(el => <div key={el.id}>
+    {this.props.userList.status === statuses.SUCCESS ? this.props.userList.data.items.map(el => <div key={el.id}>
         <hr />
         <p>{el.id} - {el.name}</p>
         <p>
